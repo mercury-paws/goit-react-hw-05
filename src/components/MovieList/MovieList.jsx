56f051c0e131f7db9d90f9b52eed8be32d748b8e
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-05-03 21:35:52 +0200

    finallydoneornot

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,15 @@
 import { Link, useLocation } from "react-router-dom";
+import css from "./MovieList.module.css";
 export default function MovieList({ movie }) {
   const location = useLocation();
 
   return (
-    <>
+    <div className={css.movieList}>
       <Link to={`/movies/${movie.id}`} state={location}>
         {movie.title}
       </Link>
       <p>Year: {movie.release_date.slice(0, 4)}</p>
       <p>Average Vote: {movie.vote_average}</p>
-    </>
+    </div>
   );
 }
